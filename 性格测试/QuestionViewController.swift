c537e6e11749247485ee39f888969c9f1a24b02c
alexscott <alexzhangshuo@163.com>
2018-04-27 23:19:16 +0800

    获得用户的答案

--- 性格测试/QuestionViewController.swift
+++ 性格测试/QuestionViewController.swift
@@ -23,15 +23,21 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel2: UILabel!
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
+    
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
     
-    
     var questionIndex = 0
+    var answersChosen: [Answer] = []
     
     var questions: [Question] = [
         Question(text: "Which food do you like the most?",
@@ -68,7 +74,59 @@ class QuestionViewController: UIViewController {
         updateUI()
     }
 
+    //MARK: Actions
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswer = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswer[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswer[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswer[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswer[3])
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswer = questions[questionIndex].answers
+        
+        let index = Int(round(rangedSlider.value * Float(currentAnswer.count - 1)))
+        answersChosen.append(currentAnswer[index])
+        
+        nextQuestion()
+    }
+    
     //MARK: Utilities
+    func nextQuestion() {
+        
+    }
+    
     //更改标题，并根据题目类型呈现不同问答方式
     func updateUI() {
         singleStackView.isHidden = true
