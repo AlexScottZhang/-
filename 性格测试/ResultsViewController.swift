72a3c08a1c080348da7d3318753bf55d8528a19d
alexscott <alexzhangshuo@163.com>
2018-04-28 00:37:07 +0800

    restart the quiz

--- 性格测试/ResultsViewController.swift
+++ 性格测试/ResultsViewController.swift
@@ -16,6 +16,8 @@ class ResultsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        
         calculatePersonalityResult()
     }
 
