ac348e7af8769a6337813d667300416af597e3c2
alexscott <alexzhangshuo@163.com>
2018-04-27 14:54:17 +0800

    Initial Commit

---
+++ 性格测试/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  性格测试
+//
+//  Created by HhhotDog on 2018/4/27.
+//  Copyright © 2018年 Alexscott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
